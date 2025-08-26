05925de26a7cd22b74e822ec99588c9cb76e6e69
unknown <rizwanhossainrahat400@gmail.com>
2025-08-27 00:27:21 +0600

    setup project and structure folder

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router'
+import './App.css'
+import CommonLayout from './components/layouts/CommonLayout'
+
+function App() {
+  return (
+    <>
+    <CommonLayout>
+    <Outlet></Outlet>
+    </CommonLayout>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return(
+         <div className="py-16 px-4 container mx-auto">
+        <h1>this is about page</h1>
+        </div>
+    );
+}
