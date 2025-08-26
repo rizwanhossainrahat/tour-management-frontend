5a2285fbdbdb2c87e5e64793e1a45884fbc6bad8
unknown <rizwanhossainrahat400@gmail.com>
2025-08-27 00:47:48 +0600

    setup project structure and folder

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,7 @@ export default function About(){
     return(
          <div className="py-16 px-4 container mx-auto">
         <h1>this is about page one </h1>
-        <h1>this is about page two </h1>
-        <h1>this is about page three </h1>
+       
         </div>
     );
 }
