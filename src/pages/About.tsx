2ae51a2fe78b3966259bd7e3b4878d8e8aaa282a
unknown <rizwanhossainrahat400@gmail.com>
2025-08-28 00:43:24 +0600

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,9 @@
+export default function About(){
+    return(
+         <div className="py-16 px-4 container mx-auto">
+        <h1>this is about page one </h1>
+        <h1>this is about page one </h1>
+       
+        </div>
+    );
+}
---
+++ src/pages/Analytics.tsx
@@ -0,0 +1,7 @@
+export default function Analytics(){
+    return(
+        <>
+        <h1>this is about analytics</h1>
+        </>
+    );
+}
