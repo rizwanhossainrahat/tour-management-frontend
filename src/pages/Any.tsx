18cdb1fca292ac9b0f2069fd953ebbbe82caa9af
unknown <rizwanhossainrahat400@gmail.com>
2025-08-28 00:52:24 +0600

    init part 2

---
+++ src/pages/Any.tsx
@@ -0,0 +1,10 @@
+export default function Any(){
+    return(
+         <div className="py-16 px-4 container mx-auto">
+        <h1>this is about page one </h1>
+        <h1>this is about page one </h1>
+        <h1>this is about page three </h1>
+       
+        </div>
+    );
+}
